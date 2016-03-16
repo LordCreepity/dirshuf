d5cbc498865b9b1ae69078b020528249b706191d
LordCreepity <johnstratton679@yahoo.com>
2016-03-16 08:20:52 -0400

    Fixed a bunch of bugs. It's not quite ready yet, however.

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 /* shuffle files in a directory by giving them random names, optionally tacking
-   a global file extension to the end */
+   a global file extension to the end. also outputs a shell script that can be
+   used to restore the original filenames */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 
-char *extension = '\0';
+char *extension;
 char *dirname;
+char eflg = 0;
 
 char *namegen();
 
@@ -15,6 +17,7 @@ int main(int argc, char **argv){
     DIR *dir;
     struct dirent *fileInDir;
     char *newName;
+    FILE *resetfile;
 
     if (argc < 2){
         fprintf(stderr, "usage: %s <directory> <optional extension>\n",
@@ -23,16 +26,28 @@ int main(int argc, char **argv){
     }
     if (argv[2] != NULL){
         extension = argv[2];
-    }
+        eflg++;
+    } 
     dirname = argv[1];
+    resetfile = fopen("./resetfile", "w");
+    if (resetfile == NULL){
+        perror("resetfile");
+        exit(2);
+    }
     dir = opendir(dirname);
     if (dir != NULL){
         while ((fileInDir = readdir(dir)) != NULL){
-            rename(fileInDir->d_name, namegen())
+            if (!strncmp(fileInDir->d_name, "resetfile", 9));
+                continue;
+            } else {
+                newName = namegen();
+                fprintf(resetfile, "mv %s %s\n", newName, fileInDir->d_name);
+                rename(fileInDir->d_name, newName);
+            }
         }
     } else {
-        perror(dirname)
-        exit(2)
+        perror(dirname);
+        exit(3);
     }
     return 0;
 }
@@ -43,12 +58,17 @@ int main(int argc, char **argv){
 
 char *namegen(){
     char *name;
-    DIR dir;
+    DIR *dir = opendir(dirname);
     struct dirent *fileInDir;
 
 doOver:
-    name = strcat(tempnam(dirname, NULL), extension);
-    while ((fileInDir == readdir(dir)) != NULL){
+    if (eflg){
+        name = strcat(tempnam(dirname, NULL), extension);
+    } else {
+        name = tempnam(dirname, NULL);
+    }
+    rewinddir(dir);
+    while ((fileInDir = readdir(dir)) != NULL){
         if (!strcmp(name, fileInDir->d_name)){
             goto doOver;
         }
